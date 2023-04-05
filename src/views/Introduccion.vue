2406c14905739e806a615478188db1e14d6f5e2b
jorozcop <orojacobo@gmail.com>
2023-04-05 17:24:56 -0500

    vinculación de videos + material descargable

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,7 +19,7 @@
     .row.mt-5(data-aos="fade-zoom")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/SRgFf52nBWc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 </template>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -229,7 +229,7 @@
     .row.mt-5(data-aos="fade-zoom")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/u7_vC0yh9wQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.justify-content-center.my-5
       .col-lg-10
